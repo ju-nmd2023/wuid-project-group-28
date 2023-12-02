36b78bcff8b928aec03a11c43650191b0d85918a
Anastasiia Makarova <maan23lp@student.ju.se>
2023-12-02 20:46:30 +0100

    filters

--- productpage.js
+++ productpage.js
@@ -7,3 +7,11 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+const filters = document.getElementsByClassName("contentBox");
+
+for (i = 0; i < filters.length; i++) {
+  filters[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
