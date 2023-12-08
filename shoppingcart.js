b278abd7406ec1062b0e78ea8816fc033b91f681
Denisa-Georgiana-23 <badeadenisa9@gmail.com>
2023-12-08 13:52:17 +0100

    shoppingcart updated

---
+++ shoppingcart.js
@@ -0,0 +1,32 @@
+/* sidebar */
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
+
+/* QUANTITY CONTROLLER*/
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.quantity-controller button').forEach(button => {
+      button.addEventListener('click', function() {
+        const direction = this.dataset.action;
+        const quantitySpan = this.closest('.quantity-controller').querySelector('.product-quantity');
+        let quantity = parseInt(quantitySpan.innerText, 10);
+  
+        if (direction === 'increase') {
+          quantity += 1;
+        } else if (direction === 'decrease' && quantity > 1) {
+          quantity -= 1;
+        }
+  
+        quantitySpan.innerText = quantity;
+      });
+    });
+  });
+  
